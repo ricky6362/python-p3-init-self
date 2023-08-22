fc149fd33cf3235f877eb38a4f293eda0f392dcb
ricky6362 <63843083+ricky6362@users.noreply.github.com>
2023-08-22 21:35:37 +0300

    Assignment done

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed = 'Mutt'):
+        self.name = name
+        self.breed = breed
+        print(f"{self.name} is born!")
+
+    def showing_self(self):
+        return self
+    
+    def test_saves_self_breed(self):
+        pass
+        
+
+    
+fido = Dog('Fido')
